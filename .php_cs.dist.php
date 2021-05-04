2ff6d7e4c6488c4f288c01083243bb3c18621e17
freek <freek@spatie.be>
2021-05-04 12:53:38 +0200

    wip

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -10,7 +10,7 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new \PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
