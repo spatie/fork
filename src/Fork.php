6db2053816bc977d7d611d84c29d89bb04a6464a
Puggan Sundragon <thorbjorn.sundragon@rolfsbuss.se>
2023-12-07 16:52:00 +0100

    Switch from kill-all-exit to a normal exit when a child is done, Issue #50

--- src/Fork.php
+++ src/Fork.php
@@ -129,7 +129,7 @@ class Fork
             try {
                 $this->executeInChildTask($task, $socketToParent);
             } finally {
-                $this->exit();
+                exit();
             }
         }
 
